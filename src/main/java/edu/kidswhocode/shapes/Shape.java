cf96abdf21de9b4d52ef7adb65cf6f1a42108fcb
Haris Waqar <harisjee1214@gmail.com>
2017-10-14 15:30:25 -0400

    adding rotate method

--- src/main/java/edu/kidswhocode/shapes/Shape.java
+++ src/main/java/edu/kidswhocode/shapes/Shape.java
@@ -1,26 +1,27 @@
 package edu.kidswhocode.shapes;
 
-public abstract class Shape
-{
-   private int x;
-   private int y;
-   private String color;
+public abstract class Shape {
+    private int x;
+    private int y;
+    private String color;
 
-   public Shape(int x, int y, String color)
-    {
-        this.x=x;
-        this.y=y;
-        this.color=color;
+    public Shape(int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
 
     }
 
     public abstract double calculateArea();
 
-   public double calculateArea(int scale)
-   {
-       return calculateArea()/scale;
+    public double calculateArea(int scale) {
+        return calculateArea() / scale;
 
-   }
+    }
+
+    public void rotate(int degrees){
+        System.out.println("Rotate is not implemented yet")
+    }
 
     @Override
     public String toString() {
